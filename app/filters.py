087b4fee476f36764a6b954216bf0b71d2a66a0c
timosahenru <timosahenru@gmail.com>
2022-09-13 03:01:16 +0000

    cleared repo

--- app/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import *
-
-
-class HouseFilter(django_filters.FilterSet):
-    class Meta:
-        model = House
-        fields = ['area', 'house_type', 'price']
-
